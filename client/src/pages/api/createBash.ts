92e925b55f833c5eb54ec9b13761e6fc3540d63e
Steven <stevensmin@gmail.com>
2023-03-24 16:45:27 -0400

    modified file names to be more exact, began implementing wrk2

--- client/src/pages/api/createBash.ts
+++ client/src/pages/api/createBash.ts
@@ -1,13 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { createBash } from '../../../../wrk_lua/test';
+import { createBash } from '../../../../wrk_lua/luaFileGenerator';
 
 export default async function scripts(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   if (req.method === 'POST') {
-    console.log(req.body);
-    const bashCreated = createBash(req.body);
+    createBash(req.body);
     res.status(200).send('bash file created successfully!');
   } else {
     res.status(400).send('Invalid request method');
--- client/src/pages/api/createLua.ts
+++ client/src/pages/api/createLua.ts
@@ -1,12 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { createLua } from '../../../../wrk_lua/test';
+import { createLua } from '../../../../wrk_lua/luaFileGenerator';
 
 export default async function scripts(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   if (req.method === 'POST') {
-    const luaCreated = createLua(req.body);
+    createLua(req.body);
     res.status(200).send('lua file created successfully!');
   } else {
     res.status(400).send('Invalid request method');
