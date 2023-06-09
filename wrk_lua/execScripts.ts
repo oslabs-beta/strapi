a8e6e4df522685ea3facd0020eb2ec797cd91662
choopi-js <stevensmin@gmail.com>
2023-06-09 16:16:24 -0400

    removed filler/unnecessary code

--- wrk_lua/execScripts.ts
+++ wrk_lua/execScripts.ts
@@ -1,21 +1,21 @@
 import { exec } from 'child_process';
 
 export default function runScript(req, res) {
-  exec('ls', (err, stdout, stderr) => {
+  exec('ls', (err) => {
     if (err) {
       console.error(`Error running script: ${err}`);
       res.status(500).send('Error running script');
       return;
     }
   });
-  exec('chmod +x execWrk2Script.sh', (err, stdout, stderr) => {
+  exec('chmod +x execWrk2Script.sh', (err) => {
     if (err) {
       console.error(`Error running script: ${err}`);
       res.status(500).send('Error running script');
       return;
     }
   });
-  exec('./execWrk2Script.sh', (err, stdout, stderr) => {
+  exec('./execWrk2Script.sh', (err) => {
     if (err) {
       console.error(`Error running script: ${err}`);
       res.status(500).send('Error running script');
